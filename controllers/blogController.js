7a036b0ec02edee1d013a364a261d9871f0e444a
Manas-tech <manastechnonjr@gmail.com>
2025-10-16 12:54:21 +0530

    Update blogController.js

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -66,7 +66,7 @@ export const getAllBlogs = async (req, res) => {
     res.status(500).json({
       success: false,
       message: 'Failed to get blogs',
-      error: process.env.NODE_ENV === 'development' ? error.message : 'Internal server error'
+      error
     });
   }
 };
